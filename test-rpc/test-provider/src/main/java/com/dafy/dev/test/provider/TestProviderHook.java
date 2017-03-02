f43648e56ca5371fc175bdc6de2250d132eeb3b4
chunxiaoli <lichunxiao@dafy.com>
2017-03-02 12:14:09 +0800

    delete created files

--- test-rpc/test-provider/src/main/java/com/dafy/dev/test/provider/TestProviderHook.java
+++
@@ -1,9 +0,0 @@
-package com.dafy.dev.test.provider;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * Hook */
-@SpringBootApplication
-public class TestProviderHook {
-}
